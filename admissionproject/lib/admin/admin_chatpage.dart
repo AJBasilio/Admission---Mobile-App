9e7d7e305423ddd79fa856b13817858f72dd9415
Dmzzzzz <dominickhanz.joson@gsfe.tupcavite.edu.ph>
2022-04-30 03:44:06 +0800

    messages

--- admissionproject/lib/admin/admin_chatpage.dart
+++ admissionproject/lib/admin/admin_chatpage.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:admissionproject/models/chatUsersModel.dart';
+import 'package:admissionproject/widgets/conversationList.dart';
+
 
 class ChatPage extends StatefulWidget {
   const ChatPage({Key? key}) : super(key: key);
@@ -7,7 +10,14 @@ class ChatPage extends StatefulWidget {
   _ChatPageState createState() => _ChatPageState();
 }
 
+
 class _ChatPageState extends State<ChatPage> {
+  List<ChatUsers> chatUsers = [
+   ChatUsers(name: "TUP_admin", messageText: "Awesome Setup", imageURL: "assets/images/logo.png", time: "Now"),
+   ChatUsers(name: "TUP_nurse", messageText: "That's Great", imageURL: "assets/images/logo.png", time: "Yesterday"),
+   ChatUsers(name: "TUP_interviewer", messageText: "Hey where are you?", imageURL: "assets/images/logo.png", time: "31 Mar"),
+    
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,13 +26,48 @@ class _ChatPageState extends State<ChatPage> {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
+            Padding(
+             padding: const EdgeInsets.only(top: 16,left: 16,right: 16),
+            child: TextField(
+            decoration: InputDecoration(
+            hintText: "Search...",
+            hintStyle: TextStyle(color: Colors.grey.shade600),
+            prefixIcon: Icon(Icons.search,color: Colors.grey.shade600, size: 20,),
+            filled: true,
+            fillColor: Colors.grey.shade100,
+            contentPadding: const EdgeInsets.all(8),
+            enabledBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(20),
+                borderSide: BorderSide(
+                    color: Colors.grey.shade100
+          )
+      ),
+    ),
+  ),
+),
+
+            ListView.builder(
+              itemCount: chatUsers.length,
+              shrinkWrap: true,
+              padding: const EdgeInsets.only(top: 16),
+              physics: const NeverScrollableScrollPhysics(),
+              itemBuilder: (context, index){
+                return ConversationList(
+                  name: chatUsers[index].name,
+                  messageText: chatUsers[index].messageText,
+                  imageUrl: chatUsers[index].imageURL,
+                  time: chatUsers[index].time,
+                  isMessageRead: (index == 0 || index == 3)?true:false,
+                );
+              },
+            ), 
             SafeArea(
               child: Padding(
                 padding: const EdgeInsets.only(left: 16,right: 16,top: 16),
                 child: Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: <Widget>[
-                    const Text("Conversations",style: TextStyle(fontSize: 32,fontWeight: FontWeight.bold),),
+                   const Text("Conversations",style: TextStyle(fontSize: 32,fontWeight: FontWeight.bold),),
                     Container(
                       padding: const EdgeInsets.only(left: 8,right: 8,top: 2,bottom: 2),
                       height: 30,
@@ -35,7 +80,7 @@ class _ChatPageState extends State<ChatPage> {
                           Icon(Icons.add,color: Colors.pink,size: 20,),
                           SizedBox(width: 2,),
                           Text("Add New",style: TextStyle(fontSize: 14,fontWeight: FontWeight.bold),),
-                          
+                            
                         ],
                       ),
                     )
