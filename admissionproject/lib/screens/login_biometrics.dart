75141aba3d3bbecf4039a5dab6024ff76ea905b0
AJ Basilio <arsherjames.basilio@gsfe.tupcavite.edu.ph>
2022-07-05 15:20:36 +0800

    Update login_biometrics.dart

--- admissionproject/lib/screens/login_biometrics.dart
+++ admissionproject/lib/screens/login_biometrics.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:admissionproject/api/local_auth_api.dart';
-import 'package:admissionproject/users/applicant/appli_home.dart';
 import 'package:url_launcher/url_launcher.dart';
 import '../models/designs/pallete.dart';
 import 'package:admissionproject/screens/main_screen.dart';
